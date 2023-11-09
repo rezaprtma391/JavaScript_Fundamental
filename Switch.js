63197d276c021ffdaa8ed94514b79f36069c4126
Muhamad Reza Pratama <rezaprtma391@gmail.com>
2023-11-10 00:53:50 +0700

    update

--- Switch.js
+++ Switch.js
@@ -14,3 +14,77 @@
 //     console.log("Not Found");
 //     break;
 // }
+
+
+// let language = "French";
+// let greeting = null;
+
+// switch(language){
+//     case "English":
+//         greeting = "Good Morning"
+//     break;
+//     case "Spanish":
+//         greeting = "Buenos Dias"
+//     break;
+//     case "French":
+//         greeting = "Bonjour"
+//     break;
+//     default:
+//         greeting = "Hello World!"
+// };
+
+// console.log(greeting); 
+
+// let language = "Indonesia";
+
+// switch(language){
+//     case "English":
+//         console.log("Good Morning")
+//         break;
+//     case "Spanish":
+//         console.log("Buenos Dias")
+//         break;
+//     case "French":
+//         console.log("Bonjour")
+//         break;
+//     case "Indonesia":
+//         console.log("Selamat Pagi")
+//         break;
+//     default:
+//         console.log("Hello World")
+// };
+
+// let i = 0
+
+// for(i=0; i <= 100; i++){
+//     console.log(i)
+// };
+
+// let umur = 18;
+
+// if(umur > 17){
+//     console.log("Boleh Membuat Ktp")
+// }else{
+//     console.log("Tidak Boleh Membuat KTP")
+// };
+
+// // Operator matematika
+// let pertambahan = 1 + 1;
+// let pengurangan = 5 - 1;
+// let perkalian = 2 * 3;
+// let bagi = 6 / 2;
+// let modulus = 9 % 4;
+// console.log(pertambahan, pengurangan, perkalian, bagi, modulus);
+
+// let harga = 5000;
+// let jml_barang = 5;
+
+// if(harga > 10000 || jml_barang > 2){
+//     console.log("Anda Mendapatkan Diskon")
+// }else{
+//     console.log("Harga dan Jumlah Barang tidak memenuhi kriteria diskon");
+// };
+
+let coba = 4 === "4";
+
+console.log(coba);
--- loop.js
+++ loop.js
@@ -172,3 +172,4 @@
 for(let i = 0; i < 20; i++){
     console.log(i)
 };
+
